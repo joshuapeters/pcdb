66a4c136520d7d85bee88b8758e4026ab187183e
JP <jp@JPs-MacBook-Air.local>
2022-09-21 18:36:33 -0400

    added email validation regex from active model example

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
 
   validates_presence_of :email, :username, :first_name, :last_name
   validates_uniqueness_of :email, :username
-  #todo: at email validation regex
+  validates_format_of :email, with: /\A(\S+)((?:[-a-z0-9]\.)[a-z]{2,})\z/i
   validates_length_of :username, :email, :in => 4..50
 end
